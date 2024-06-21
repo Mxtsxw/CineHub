41c8534e89c61c0f40bc869078ebc41ab1dda36e
Matthieu <randriantsoamatthieu@gmail.com>
2024-06-21 16:41:23 +0200

    feature: browse actors

--- src/app/shared/components/actor-card/actor-card.component.ts
+++ src/app/shared/components/actor-card/actor-card.component.ts
@@ -1,12 +1,14 @@
 import {Component, Input} from '@angular/core';
 import {Actor} from "../../../models/actor";
 import {NgForOf} from "@angular/common";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-actor-card',
   standalone: true,
   imports: [
-    NgForOf
+    NgForOf,
+    RouterLink
   ],
   templateUrl: './actor-card.component.html',
   styleUrl: './actor-card.component.scss'
